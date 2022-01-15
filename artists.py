df72fff6276970fb1f93148596af98ffd467ad53
Jonathan Yin <jonathanyin.12@gmail.com>
2022-01-15 15:45:04 -0500

    Create artists.py

---
+++ artists.py
@@ -0,0 +1,14 @@
+from utils import sp, convert_to_df
+
+def get_artist_data(artist_id):
+    artist = sp.artist(artist_id)
+    fields = ['name', 'genres', 'popularity']
+    artist_meta = [artist[field] for field in fields] + [artist['followers']['total']]
+    return artist_meta
+
+
+def get_top_artists(time_range = 'long_term', limit=50):
+    top_artist = sp.current_user_top_artists(limit=limit, offset=0, time_range=time_range)['items']
+    artists_data = [get_artist_data(artist['id']) for artist in top_artist]
+    artist_df = convert_to_df(artists_data, columns=['name', 'genre', 'popularity', 'followers'])
+    return artist_df
