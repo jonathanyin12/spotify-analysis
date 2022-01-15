d64697d253733c54adeb6b0bb73e409926984396
Jonathan Yin <jonathanyin.12@gmail.com>
2022-01-15 15:44:46 -0500

    Use loky backend

--- scrape.py
+++ scrape.py
@@ -5,13 +5,9 @@ from utils import convert_to_df, sp
 
 def get_spotify_songs():
     playlist_ids = get_spotify_playlists()
-    all_tracks_ids= Parallel(n_jobs=-1,backend='threading', verbose=8)(delayed(get_playlist_tracks_ids)(id) for id in playlist_ids)
+    
+    all_tracks_ids= Parallel(n_jobs=-1, verbose=8)(delayed(get_playlist_tracks_ids)(id) for id in playlist_ids)
     all_tracks_ids = list(set(sum(all_tracks_ids, [])))
-
-    # all_track_ids = set()
-    # for id in tqdm(playlist_ids):
-    #     track_ids = get_playlist_tracks_ids(id)
-    #     all_track_ids.update(track_ids)
 
     tracks_data = get_tracks_data(all_tracks_ids)
     df = convert_to_df(tracks_data, columns= track_meta_cols+track_feature_cols)
