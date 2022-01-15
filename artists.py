fa33a3c6f18a81a04a403cceffde8b130ca53a7a
Jonathan Yin <jonathanyin.12@gmail.com>
2022-01-15 17:18:04 -0500

    Add top genres

--- artists.py
+++ artists.py
@@ -1,5 +1,6 @@
 from utils import convert_to_df
 from config import sp
+from collections import Counter
 
 def get_artist_data(artist_id):
     artist = sp.artist(artist_id)
@@ -9,7 +10,20 @@ def get_artist_data(artist_id):
 
 
 def get_top_artists(time_range = 'long_term', limit=50):
-    top_artist = sp.current_user_top_artists(limit=limit, offset=0, time_range=time_range)['items']
-    artists_data = [get_artist_data(artist['id']) for artist in top_artist]
+    top_artists = sp.current_user_top_artists(limit=limit, offset=0, time_range=time_range)['items']
+    artists_data = []
+    fields = ['name', 'genres', 'popularity']
+    for artist in top_artists:
+        artist_meta = [artist[field] for field in fields] + [artist['followers']['total']]
+        artists_data.append(artist_meta)
     artist_df = convert_to_df(artists_data, columns=['name', 'genre', 'popularity', 'followers'])
     return artist_df
+
+
+def get_top_genres(time_range = 'long_term', limit=10):
+    genres = []
+    top_artist = sp.current_user_top_artists(limit=50, offset=0, time_range=time_range)['items']
+    for artist in top_artist:
+        genres.extend(artist['genres'])
+    genres = Counter(genres)
+    return genres.most_common(n = limit)
